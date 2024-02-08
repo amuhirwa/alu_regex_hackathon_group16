6f330a7c1f8e8f00f75d764becd63fb019622876
amuhirwa <144004199+amuhirwa@users.noreply.github.com>
2024-02-08 15:34:17 +0200

    Create regex.py

---
+++ regex.py
@@ -0,0 +1,9 @@
+import re
+
+if __name__ == "__main__":
+    test_string = input("Enter your data: ")
+    list_of_patterns = [r'.+\s\(\d{4}\)', r'\[Verse\s\d+\].+']
+    list_of_matches = []
+    for pattern in list_of_patterns:
+        list_of_matches.append(re.findall(pattern ,test_string))
+    print('Search results: ',list_of_matches)
