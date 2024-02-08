9e964381fb9f0bd9bf0e5ce52072eed139efb9f0
amuhirwa <144004199+amuhirwa@users.noreply.github.com>
2024-02-08 15:36:02 +0200

    Update regex.py

--- regex.py
+++ regex.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     list_of_patterns = [r'.+\s\(\d{4}\)', r'\[Verse\s\d+\].+']
     list_of_matches = []
     for pattern in list_of_patterns:
-        list_of_matches.append(re.findall(pattern ,test_string))
-    print('Search results: ',list_of_matches)
+        list_of_matches.append(re.findall(pattern, test_string))
+    print('Search results: ', list_of_matches)
