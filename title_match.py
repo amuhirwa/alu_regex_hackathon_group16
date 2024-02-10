c7199615f50f2ce2f2f699c57a26eb254a63c9f9
amuhirwa <144004199+amuhirwa@users.noreply.github.com>
2024-02-10 11:00:11 +0200

    Update title_match.py

--- title_match.py
+++ title_match.py
@@ -2,6 +2,6 @@ import re
 
 if __name__ == "__main__":
     test_string = input("Enter your data: ")
-    patterns = r'.+\s\(\d{4}\)
+    patterns = r'.+\s\(\d{4}\)'
     matches = re.findall(pattern, test_string)
     print('Search results: ', matches)
