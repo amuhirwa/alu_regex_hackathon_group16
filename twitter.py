8eb4ec7d0a7b4ce65c379a01e882e1ca346836c3
amuhirwa <144004199+amuhirwa@users.noreply.github.com>
2024-02-10 12:49:24 +0200

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -2,6 +2,6 @@ import re
 
 if __name__ == "__main__":
     test_string = input("Enter your data: ")
-    patterns = r'@[\w\d]+'
+    patterns = r'@w+'
     matches = re.findall(patterns, test_string)
     print('Search results: ', matches)
