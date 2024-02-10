261014a54e5b18053d1c9c597cfead33e0146077
Pnayiturik <144005060+Pnayiturik@users.noreply.github.com>
2024-02-10 11:16:31 +0200

    Update song_lyrics.py

--- song_lyrics.py
+++ song_lyrics.py
@@ -3,5 +3,5 @@ import re
 if __name__ == "__main__":
     test_string = input("Enter your data: ") 
     patterns = r'\[Verse\s+\d+\].+'
-    results = re.findall(patterns, test_string)
-    print('Search results: ', results)
+    matches = re.findall(patterns, test_string)
+    print('Search results: ', matches)
