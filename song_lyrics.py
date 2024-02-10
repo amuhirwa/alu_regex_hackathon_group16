76d5688b5892313f2f71662ad94c22e3913300e9
amuhirwa <144004199+amuhirwa@users.noreply.github.com>
2024-02-10 10:56:05 +0200

    Update song_lyrics.py

--- song_lyrics.py
+++ song_lyrics.py
@@ -1,1 +1,7 @@
+import re
 
+if __name__ == "__main__":
+    test_string = input("Enter your data: ")
+    patterns = r'\[Verse\s+\d+\].+'
+    matches = re.findall(pattern, test_string)
+    print('Search results: ', matches)
