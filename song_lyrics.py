50baf7bfcf7eaa4cf9962226eaa6df5d3410ab79
lscblack <l.christian@alustudent.com>
2024-02-11 15:09:06 +0200

    All Done In One

--- song_lyrics.py
+++ song_lyrics.py
@@ -4,4 +4,7 @@ if __name__ == "__main__":
     test_string = input("Enter your data: ") 
     patterns = r'\[Verse\s+\d+\].+'
     matches = re.findall(patterns, test_string)
-    print('Search results: ', matches)
+    if matches:
+        print('Search results: ', matches)
+    else:
+        print('No match found!')
--- title_match.py
+++ title_match.py
@@ -4,5 +4,8 @@ if __name__ == "__main__":
     test_string = input("Enter your data: ")
     patterns = r'.+\s\(\d{4}\)'
     matches = re.findall(patterns, test_string)
-    print('Search results: ', matches)
+    if matches:
+        print('Search results: ', matches)
+    else:
+        print('No match found!')
 
